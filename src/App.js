6756a7a702d020dfcf8f6a3936feae4802d5b7b1
Jon  Wang <jonathanwang@Jons-MacBook-Pro.local>
2017-08-21 13:58:45 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Fucking App</h2>
+          <h2>Test App</h2>
         </div>
         <div>
           <SearchBar />
